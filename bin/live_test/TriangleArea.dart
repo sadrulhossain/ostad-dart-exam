5aac055e026f2e2bff3c7da48fb05e718e45d2fa
Sadrul Hossain <hossainsadrul@gmail.com>
2025-01-01 23:13:59 +0600

    OFC-02 Feat: completed 1st live test.

---
+++ bin/live_test/TriangleArea.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  try {
+    print("Let's calculate the area of the triangle");
+    print("Enter length of base:");
+    int? base = int.tryParse(stdin.readLineSync()!);
+    print("Enter length of height:");
+    int? height = int.tryParse(stdin.readLineSync()!);
+    print("The area of the triangle: ${triangle(base, height)}");
+  } catch (e) {
+    print("Oops, something went wrong. [Reason] : $e");
+  }
+}
+
+double triangle(base, height) {
+  return 0.5 * base * height;
+}
